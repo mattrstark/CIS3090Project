76c9e22a258072b88695e6a2d02ff4875a9e3634
Matt Stark <mstark@uoguelph.ca>
2013-11-07 23:15:51 -0500

    Adding multiprocessing and command line

--- piltest.py
+++ piltest.py
@@ -1,4 +1,10 @@
 from PIL import Image
+import time
+import sys
+import multiprocessing, logging
+logger = multiprocessing.log_to_stderr()
+logger.setLevel(multiprocessing.SUBDEBUG)
+
 
 global assetDir
 global saveDir
@@ -7,26 +13,33 @@ assetDir = "assets/"
 saveDir = "results/"
 ext = ".jpg"
 
-def imgResize(im):
-
+def imgResize(imageName):
+    im1 = Image.open(imageName).convert("RGB")
     div = 2
-    width = im.size[0] / div
-    height = im.size[1] / div
+    width = im1.size[0] / div
+    height = im1.size[1] / div
 
-    im2 = im.resize((width, height), Image.NEAREST) # use nearest neighbour
-    im3 = im.resize((width, height), Image.BILINEAR) # linear interpolation in a 2x2 environment
-    im4 = im.resize((width, height), Image.BICUBIC) # cubic spline interpolation in a 4x4 environment
-    im5 = im.resize((width, height), Image.ANTIALIAS) # best down-sizing filter
+    im2 = im1.resize((width, height), Image.NEAREST) # use nearest neighbour
+    im3 = im1.resize((width, height), Image.BILINEAR) # linear interpolation in a 2x2 environment
+    im4 = im1.resize((width, height), Image.BICUBIC) # cubic spline interpolation in a 4x4 environment
+    im5 = im1.resize((width, height), Image.ANTIALIAS) # best down-sizing filter
     
     im2.save(saveDir + "NEAREST" + ext)
     im3.save(saveDir + "BILINEAR" + ext)
     im4.save(saveDir + "BICUBIC" + ext)
     im5.save(saveDir + "ANTIALIAS" + ext)
 
-imageFile = assetDir + "test.jpg"
-im1 = Image.open(imageFile).convert("RGB")
 
-imgResize(im1)
-
+if __name__ == '__main__':
+    #pool = Pool(processes=2)
+    if len(sys.argv) < 2:
+        sys.exit("Not enough arguments provided")
+    time.clock
+    imageName = assetDir + sys.argv[1]
+    
+    worker = multiprocessing.Process(target=imgResize, args=(imageName,)) # use default name
+    worker.start()
+    #print pool.map(imgResize, im1)
+    print "Done in %.2f seconds" % time.clock()
 
 
